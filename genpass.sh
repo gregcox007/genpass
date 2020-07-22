7a20dd586471cfd24b81c9617a1b713751830fe8
gregcox007 <63885007+gregcox007@users.noreply.github.com>
2020-07-22 08:56:41 -0600

    Update genpass.sh
    Update to remove potential script breaking special characters to make AEM compliant

--- genpass.sh
+++ genpass.sh
@@ -10,4 +10,4 @@ else
 fi
 
 #Generate Password with Random Upper/Lower Case as well as symbols then copies to clipboard
-LC_ALL=C tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c "$stglength" | pbcopy
+LC_ALL=C tr -dc 'A-Za-z0-9''@#%*-+=~[]{}<>?' </dev/urandom | head -c "$stglength" | pbcopy
